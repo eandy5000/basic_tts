67ffc72505f67b3213cd4f1e365ab820e85bdd58
Andy Eccleston <eandy5000@yahoo.com>
2016-11-10 14:50:29 -0600

    working on speech.js extracting document properties

--- public/scripts/speech.js
+++ public/scripts/speech.js
@@ -1,4 +1,25 @@
 var div = document.querySelector(".talk")
 var but = document.querySelector("player")
 
-console.log(div)
+
+//info our reader will need from .talk
+var talkInfo = function() {
+   var out = {}
+//see if .talk has children
+   out.childCheck = (div.children.length >= 1) ? true: false
+   out.childNumber = div.children.length
+   out.childArray = div.children
+   return out
+
+}
+
+var nodeChecker = function(obj) {
+    for (var key in obj) {
+        console.log(key, obj[key])
+    }
+}
+
+
+var info = talkInfo()
+
+console.log(nodeChecker(info.childArray))
