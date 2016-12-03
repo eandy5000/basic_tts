76e85562df4269023ee8885730b69cc15421c778
Andy Eccleston <eandy5000@yahoo.com>
2016-12-03 15:04:32 -0600

    current output works but I need to use dom methods and for of

--- public/scripts/speech.js
+++ public/scripts/speech.js
@@ -1,4 +1,37 @@
 var div = document.querySelector(".talk")
 var but = document.querySelector(".player")
 
+//testing DOM api 
 
+//children
+console.log(div.children.length)
+console.log(div.children)
+
+//HTMLcollection.item()
+for(var i = 0; i < div.children.length; i++) {
+    console.log(div.children.item(i))
+}
+
+//testing
+console.log(div.children.item(1).childNodes)
+
+//gets div children child nodes
+var deepPull = function (item) {
+    var nodes = item.childNodes
+    console.log("div child nodes ",nodes, " keys ", nodes.keys())
+}
+
+//seperates multiple div children
+var puller = function(item) {
+
+    var children = item.children
+
+    for (var i = 0; i < children.length; i++) {
+        console.log((i + 1), "\tdiv children ", children.item(i))
+        deepPull(children.item(i))
+    }
+
+
+}
+
+puller(div)
